9dea23213b8263fe6d84df51232da2feba0f317f
mayankjain2710 <158475523+mayankjain2710@users.noreply.github.com>
2024-07-05 15:55:43 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//backgroundImage: `url(https://images.pexels.com/photos/5126268/pexels-photo-5126268.jpeg)`,
